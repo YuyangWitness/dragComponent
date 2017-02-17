658a48fe54b53e5ec40b7f2d19686515897c98ed
Yuyang <jxndyy2014@163.com>
2017-02-17 11:14:10 +0800

    第二次修改
    1、增加自动计算和新增滑块功能
    2、增加滑动效果功能

--- js/dragComponent.js
+++ js/dragComponent.js
@@ -1,30 +1,55 @@
 (function(w, d) {
 	function Drag(setting) { //setting : data,stageEle,
-		this.setting = {}
+		this.setting = {};
+		//	遍历传入信息
 		for(val in setting) {
 			this.setting[val] = setting[val];
 		}
-
+		//初始化，把data放入到stage里面
 		if(this.setting.data != null && this.setting.stageEle != null) {
 			var i = 0,
 				len = this.setting.data.length,
 				str = null,
 				stageEleHtml = this.setting.stageEle.innerHTML;
-
-			for(; i < len; i++) {
-				var str = '<div class="dragVal">' + this.setting.data[i] + '</div>';
-				stageEleHtml = stageEleHtml + str;
+			if(this.setting.index != null) {
+				for(; i < len; i++) {
+					var str = '<div class="dragVal" data-index="' + this.setting.index[i] + '">' + this.setting.data[i] + '</div>';
+					stageEleHtml = stageEleHtml + str;
+				}
+			} else {
+				for(; i < len; i++) {
+					var str = '<div class="dragVal">' + this.setting.data[i] + '</div>';
+					stageEleHtml = stageEleHtml + str;
+				}
 			}
 
 			this.setting.stageEle.innerHTML = stageEleHtml;
 
+			this.setWidth(); //自动计算dragVal和drag的width并建立
+
 		}
 
 	}
+	//自动计算width
+	Drag.prototype.setWidth = function() {
+		var dragVal = document.getElementsByClassName("dragVal");
+		var Width = 100 / dragVal.length;
+		var i = 0,
+			len = dragVal.length;
+		for(; i < len; i++) {
+			dragVal[i].style.width = Width + "%";
+		}
+		var dragDiv = dragVal[0].previousElementSibling;
+		dragDiv.style.width = Width + "%";
+		setHtml(dragDiv, dragVal[0]); //初始化选中名称
 
+	}
+
+	//拖拽开始
 	Drag.prototype.drag = function(dragEle) {
-		console.log("进来了");
+		//console.log("进来了");
 		var _this = this;
+		var dragVal = document.getElementsByClassName("dragVal");
 		if(!this.setting.stageEle) {
 			throw Error("this is not defined stageEle");
 		} else {
@@ -34,28 +59,20 @@
 				var disx = e.clientX - this.offsetLeft;
 				var disy = e.clientY - this.offsetTop;
 				var dragging = true;
+				var Left = 0;
+				var Top = 0;
 
-				//如果提供了事件对象，则这是一个非IE浏览器 
-				if(e && e.preventDefault) {
-					//阻止默认浏览器动作(W3C) 
-					e.preventDefault();
-				} else {
-					window.event.returnValue = false;
-				}
-				//如果提供了事件对象，则这是一个非IE浏览器
-				if(e && e.stopPropagation) {
-					//因此它支持W3C的stopPropagation()方法
-					e.stopPropagation();
-				} else {
-					//否则，我们需要使用IE的方式来取消事件冒泡 
-					window.event.cancelBubble = true;
-				}
+				//阻止默认事件
+				preventDefault(e);
+
+				//阻止冒泡
+				stopPro(e);
 
 				d.addEventListener("mousemove", function(e) {
 					if(dragging) {
 						var e = e || event;
-						var Left = e.clientX - disx;
-						var Top = e.clientY - disy;
+						Left = e.clientX - disx;
+						Top = e.clientY - disy;
 
 						if(Left < 2) {
 							Left = 2;
@@ -68,6 +85,11 @@
 						} else if(Top > _this.setting.stageEle.clientHeight - dragEle.offsetHeight - 2) {
 							Top = _this.setting.stageEle.clientHeight - dragEle.offsetHeight - 2;
 						}
+						//console.log(Math.round(Left/dragEle.offsetWidth));
+						//转换drag中的文字
+						var index = Math.round(Left / dragEle.offsetWidth);
+
+						setHtml(dragEle, dragVal[index]);
 
 						dragEle.style.left = Left + 'px';
 						dragEle.style.top = Top + 'px';
@@ -76,11 +98,51 @@
 
 				document.addEventListener("mouseup", function() {
 					dragging = false;
+					var index = Math.round(Left / dragEle.offsetWidth);
+					console.log(dragEle.offsetWidth);
+					console.log(_this.setting.stageEle.clientWidth);
+					if(index == 0) {
+						dragEle.style.left = (index * dragEle.offsetWidth + 2) + 'px';
+					} else if(index == dragVal.length - 1) {
+						dragEle.style.left = (index * dragEle.offsetWidth - 2) + 'px';
+					} else {
+						dragEle.style.left = index * dragEle.offsetWidth + 'px';
+					}
+
 				});
 
 			});
 		}
 
+	}
+
+	/*兼容性   设置text*/
+	function setHtml(ele, setEle) { //ele被设置对象,setEle设置对象
+		if(ele.textContent != null) {
+			ele.textContent = setEle.textContent;
+		} else {
+			ele.innerText = setEle.innerText;
+		}
+	}
+
+	/*兼容性  阻止默认事件*/
+	function preventDefault(e) {
+		var e = e || window.event;
+		if(e.preventDefault && e) {
+			e.preventDefault()
+		} else {
+			window.event.returnValue = false;
+		}
+	}
+
+	/*兼容性  阻止冒泡*/
+	function stopPro(e) {
+		var e = e || window.event;
+		if(e.stopPropagation && e) {
+			e.stopPropagation();
+		} else {
+			window.event.cancelBubble = true;
+		}
 	}
 
 	window.Drag = Drag;
